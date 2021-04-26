ac7ae106be84afd188bc9d3a0a5146687e7de76e
Raya22 <raya.lobany94@gmail.com>
2021-04-26 20:27:54 +0300

    third

--- js/app.js
+++ js/app.js
@@ -3,11 +3,13 @@
 let itemsNamesArray = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 let itemsSrcArray=['/img/bag.jpg', '/img/banana.jpg', '/img/bathroom.jpg', '/img/boots.jpg', '/img/breakfast.jpg', '/img/bubblegum.jpg', '/img/chair.jpg', '/img/cthulhu.jpg', '/img/dog-duck.jpg', '/img/dragon.jpg', '/img/pen.jpg', '/img/pet-sweep.jpg', '/img/scissors.jpg', '/img/shark.jpg', '/img/sweep.png', '/img/tauntaun.jpg', '/img/unicorn.jpg', '/img/usb.gif', '/img/water-can.jpg', '/img/wine-glass.jpg'];
 
-let results = document.getElementById('items');
+let ImgsContainer = document.getElementById('items');
 let rightImg =document.getElementById('right');
 let centerImg =document.getElementById('center');
 let leftImg =document.getElementById('left');
-let button = document.getElementById('ViewResults');
+let clickList=document.getElementById('list');
+let Resultbutton=document.getElementById('ViewResults');
+let ulElement=document.getElementById('list');
 
 let totalClicks = 0;
 
@@ -24,18 +26,20 @@ function Items(name, src) {
 
 
 function randomNumber() {
-  let Random= Math.floor(Math.random() * Items.pushedItemsArray.length);
-  return Random;
-  
+  return Math.floor(Math.random() * Items.pushedItemsArray.length);
 }
 
 function CreatItems() {
   for(let i = 0; i < itemsNamesArray.length; i++) {
-    let NewItem= new Items(itemsNamesArray[i], itemsSrcArray[i]);
+    new Items(itemsNamesArray[i], itemsSrcArray[i]);
 
   }
 }
 
+
+let leftclick =0;
+let centerclick =0;
+let rightclick =0;
 
 
 function displayItems() {
@@ -47,36 +51,70 @@ function displayItems() {
     }
   }
 
-leftImg.src = Items.pushedItemsArray[Items.previousCurrentItems[0]].src;
+
+  // here I choose a random number from the stored random which is in the previousCurrent[] array
+  // then I pass this number To PushedItemsArray [] which contain all Items object to choose the src for the same Image
+  leftImg.src = Items.pushedItemsArray[Items.previousCurrentItems[0]].src;
+  //here I access the TimeShown for the same image to increase it by one
   Items.pushedItemsArray[Items.previousCurrentItems[0]].timesShown++;
-  leftImg = Items.pushedItemsArray[Items.previousCurrentItems[0]];
+  leftclick = Items.pushedItemsArray[Items.previousCurrentItems[0]];
 
   centerImg.src = Items.pushedItemsArray[Items.previousCurrentItems[1]].src;
   Items.pushedItemsArray[Items.previousCurrentItems[1]].timesShown++;
-  centerImg = Items.pushedItemsArray[Items.previousCurrentItems[1]];
+  centerclick = Items.pushedItemsArray[Items.previousCurrentItems[1]];
 
   rightImg.src = Items.pushedItemsArray[Items.previousCurrentItems[2]].src;
   Items.pushedItemsArray[Items.previousCurrentItems[2]].timesShown++;
-  rightImg = Items.pushedItemsArray[Items.previousCurrentItems[2]];
+  rightclick = Items.pushedItemsArray[Items.previousCurrentItems[2]];
 
- 
+
   Items.previousCurrentItems = Items.previousCurrentItems.slice(3, 6);
 }
 
-function ViewRes() {
-  let header1 = document.createElement('h1');
-  header1.textContent = 'Stats';
-  results.appendChild(header1);
-
-  let buttonRes = document.createElement('a');
-  buttonRes.textContent = 'ViewResults';
-  buttonRes.href = '#status';
-  button.appendChild(buttonRes);
-
-  for (let i = 0; i < Items.pushedItemsArray.length; i++) {
-    let header1 = document.createElement('li');
-    header1.textContent = Items.pushedItemsArray[i].clicks + ' votes for ' + Items.pushedItemsArray[i].name;
-    results.appendChild(header1);
+let clickHandler = function(event) {
+  if (event.target === ImgsContainer) {
+    return alert('please click on the image');
   }
+  totalClicks++;
+  let clickedItem = event.target;
+  let id = clickedItem.id;
+  if (id === 'right'){
+    rightclick.clicks++;
+  }
+  if (id === 'center'){
+    centerclick.clicks++;
+  }
+  if (id === 'left'){
+    leftclick.clicks++;
+  }
+
+
+  if(totalClicks === 25) {
+    ViewListFunction();
+  }
+};
+
+
+
+ImgsContainer.addEventListener('click', clickHandler);
+
+function ViewListFunction(event) {
+  for(let i=0; i<Items.pushedItemsArray.length;i++)
+  {
+    let liElement=document.createElement('li');
+    ulElement.appendChild(liElement);
+    liElement.textContent=Items.pushedItemsArray[i].name+'had a '+Items.pushedItemsArray[i].clicks+'votes , and was seen a '+Items.pushedItemsArray[i].timesShown;
+
+  }
+  Resultbutton.removeEventListener('click',ViewListFunction);
 }
+
+Resultbutton.addEventListener('click',ViewListFunction);
+
+CreatItems();
+
+displayItems();
+
+
+
 
